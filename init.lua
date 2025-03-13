510c9c9dd97a525df39871b33d0ef9b561640e90
Sandro Belfanti <sandro.belfanti@chipmind.ai>
2025-03-13 16:59:55 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,19 @@
+vim.o.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.opt.termguicolors = true
+vim.cmd('colorscheme zaibatsu')
+
+vim.opt.wrap = false
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.completeopt = "menuone,noselect"
